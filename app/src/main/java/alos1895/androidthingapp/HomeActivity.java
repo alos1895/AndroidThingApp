bcd8f1c3d4613cdc66bafa39296c8061983a006c
alos1895 <alos1895@gmail.com>
2018-11-29 17:46:40 -0600

    Initial commit

---
+++ app/src/main/java/alos1895/androidthingapp/HomeActivity.java
@@ -0,0 +1,103 @@
+package alos1895.androidthingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class HomeActivity extends Activity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    private Gpio mLedGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Log.i(TAG, "Starting ButtonActivity");
+        PeripheralManager pioService = PeripheralManager.getInstance();
+        try {
+            Log.i(TAG, "Configuring GPIO pins");
+            mLedGpio = pioService.openGpio(BoardDefaults.getGPIOForLED());
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e(TAG, "Error configuring GPIO pins", e);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Runnable ledBlinker = new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    // Turn on the LED
+                    setLedValue(true);
+                    sleep(3000);
+                    // Turn off the LED
+                    setLedValue(false);
+                    sleep(3000);
+                }
+            }
+        };
+        new Thread(ledBlinker).start();
+    }
+
+    private void sleep(int milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Update the value of the LED output.
+     */
+    private void setLedValue(boolean value) {
+        try {
+            mLedGpio.setValue(value);
+        } catch (IOException e) {
+            Log.e(TAG, "Error updating GPIO value", e);
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED GPIO", e);
+            } finally{
+                mLedGpio = null;
+            }
+            mLedGpio = null;
+        }
+    }
+}
